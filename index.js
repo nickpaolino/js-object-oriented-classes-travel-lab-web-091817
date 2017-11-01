7dc1e8271bb5fef3ee1a966e49a4df0d60b2d3cc
Nick Paolino <github email address>
2017-11-01 10:12:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year){
+    return (year - this.startDate.getFullYear()) - 1;
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let vertical_distance = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+    let horizontal_distance = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+    return horizontal_distance + vertical_distance;
+  }
+
+  estimatedTime(peakHours){
+    if (peakHours){
+      return this.blocksTravelled() / 2
+    }
+    else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
